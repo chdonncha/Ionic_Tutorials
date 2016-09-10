40a3db3cd8fd921f5caf26c6af4ff0df31c550fc
chdonncha <chdonncha@gmail.com>
2016-09-10 01:15:02 +0100

    added controller

--- www/js/app.js
+++ www/js/app.js
@@ -6,7 +6,11 @@
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
 
-angular.module('ionicApp', ['ionic']);
+angular.module('ionicApp', ['ionic'])
+.controller("MainCtrl",function(){
+  console.log("Main Controller says: Hello World");
+});
+
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
